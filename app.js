28f78b6b7984ea581eb7f7d448f8a636287151cb
Eloy Cepa Andrés <eloycepa@gmail.com>
2016-06-27 18:38:34 +0200

    tracks v1.5

--- app.js
+++ app.js
@@ -1,60 +1,45 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var methodOverride = require('method-override');
+var express = require("express"),  
+    app = express(),
+    bodyParser  = require("body-parser"),
+    http     = require("http"),
+    server   = http.createServer(app),
+    methodOverride = require("method-override");
 
-var routes = require('./routes/index');
-
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', routes);
+app.use(bodyParser.urlencoded({ extended: false }));  
+app.use(bodyParser.json());  
+app.use(methodOverride());
 
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+var track_controller = require('./controllers/tracks_controller');
+
+
+/*LANDING PAGE*/
+var landingRouter = express.Router();
+landingRouter.get('/', function(req, res){
+  res.send("Bienvenido a la API de tracks");
 });
+app.use(landingRouter);
 
-// error handlers
 
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
+/* RUTAS API */
+var router = express.Router();
 
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+router.route('/tracks')
+  .post(track_controller.addTrack);
+
+router.route('/tracks/:name')
+  .get(track_controller.findTrackByName)
+  .post(track_controller.deleteTrackByName);
+
+/***OJO, CAMBIAR LA RUTA tracks.cdpsfy.com **/
+app.use('/api', router);
+
+app.listen(3030, function() {  
+    console.log("Node server running on http://localhost:3030");
 });
 
 
-module.exports = app;
+
+
+
+
--- controllers/track_controller.js
+++ controllers/track_controller.js
@@ -1,28 +1,30 @@
 var fs = require('fs');
 
+
+
 //GET - Retorna una cancion por su nombre
 exports.findTrackByName = function(req, res){
-	var findURL = req.params.name;
+    var findURL = req.params.name;
 
-	//recogemos el fichero con ese nombre dentro del NAS
+    //recogemos el fichero con ese nombre dentro del NAS
     //PRODUCCION: var urlNAS = "../mnt/nas/";
-	var urlNAS = "../mnt/nas/";
-	var newURL = urlNAS+findURL;
+    var urlNAS = "../mnt/nas/";
+    var newURL = urlNAS+findURL;
 
-	//mando la cancion que se encuentra en esa ruta:
-	res.sendFile(findURL,{root: '../mnt/nas'});
+    //mando la cancion que se encuentra en esa ruta:
+    res.sendFile(findURL,{root: '../mnt/nas'});
 };
 
 
 //POST - Inserta una nueva cancion en la DB
 exports.addTrack = function(req, res){
-	var urlNAS = "/mnt/nas/";
+    var urlNAS = "../mnt/nas/";
 
-	if (req.method == 'POST') {
-		var fileName = '';
-		var mp3File = '';
-    	var mp3_file;
-    	var tempName = '';
+    if (req.method == 'POST') {
+        var fileName = '';
+        var mp3File = '';
+        var mp3_file;
+        var tempName = '';
 
         var body = '';
         var contador = 0;
@@ -30,42 +32,37 @@ exports.addTrack = function(req, res){
             body += data;
 
             if (contador == 0){
-            	var stringData = data.toString();
-                console.log(stringData);
-            	stringData = stringData.substr(stringData.indexOf('filename')+13);
+                var stringData = data.toString();
+
+                stringData = stringData.substr(stringData.indexOf('filename')+13);
                 
-            	stringData = stringData.substr(0,stringData.indexOf('.')+4);
+                stringData = stringData.substr(0,stringData.indexOf('.')+4);
     
-            	filename = stringData;
-                console.log(filename);
-            	var random = Math.floor((Math.random() * 100) + 1);
+                filename = stringData;
+
+                var random = Math.floor((Math.random() * 100) + 1);
 
                 console.log("FILENAME: "+filename);
-                //if (fileName == ""){
-                //    filename = ".mp3";
-                //}
-                console.log("filename :"+filename);
-                //urlNAS = "../mnt/nas/"
-            	tempName = new Date().getTime()+random+'_'+filename;
-                //filename por tempname
-            	mp3File = urlNAS + fileName;
+                if (fileName == ""){
+                    filename = ".mp3";
+                }
 
-                console.log('Hasta aqui funciona');
-            	mp3_file = fs.createWriteStream(mp3File);
-            	mp3_file.write(data);
-                console.log('==========================================');
-            	contador++;
+                tempName = new Date().getTime()+random+'_'+filename;
+                mp3File = urlNAS + tempName;
+
+                mp3_file = fs.createWriteStream(mp3File);
+                mp3_file.write(data);
+                contador++;
             }else{
-            	mp3_file.write(data);
+                mp3_file.write(data);
             }
         });
         req.on('end', function () {
-        	console.log("TERMINADO");
+            console.log("TERMINADO");
 
             mp3_file.end();
             res.writeHead(200, {'Content-Type': 'text/html'});
-            //filename por tempname
-        	res.end(filename);
+            res.end(tempName);
         });
 
     }
@@ -74,13 +71,13 @@ exports.addTrack = function(req, res){
 
 //DELETE - Borramos una cancion por su nombre
 exports.deleteTrackByName = function(req,res){
-	var urlNAS = "../mnt/nas/";
-	var findURL = req.params.name;
-	var newURL = urlNAS+findURL;
+    var urlNAS = "../mnt/nas/";
+    var findURL = req.params.name;
+    var newURL = urlNAS+findURL;
 
-	//borramos el fichero en esa ruta
-	var fs = require('fs');
-	fs.unlinkSync(newURL);
-	res.status(200);
-	console.log("DELETED:"+findURL);
+    //borramos el fichero en esa ruta
+    var fs = require('fs');
+    fs.unlinkSync(newURL);
+    res.status(200);
+    console.log("DELETED:"+findURL);
 };
